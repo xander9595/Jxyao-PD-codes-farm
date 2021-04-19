640f6c667de49660025ae752fa14ebf847fea40b
xander9595 <44404218+xander9595@users.noreply.github.com>
2021-04-19 19:07:44 +0800

    Add files via upload

---
+++ pd2d-bondbased-adr-rotation/createCoords.jl
@@ -0,0 +1,45 @@
+using LinearAlgebra
+
+function createCoords(n1::Int, n2::Int, x, y)
+
+    #Fill coords array
+    nbnd = 3
+    coords = zeros(2, n1 * (n2 + 2 * nbnd))
+    dx = x / n1
+    cnt = 0
+
+    for i = 1:n2
+        for j = 1:n1
+            cnt = cnt + 1
+            tmprad = norm([(-x / 2) + dx / 2 + (j - 1) * dx, (-y / 2) + dx / 2 + (i - 1) * dx])
+            #if tmprad > 0.005
+            @inbounds coords[1:2, cnt] =
+                [(-x / 2) + dx / 2 + (j - 1) * dx, (-y / 2) + dx / 2 + (i - 1) * dx]
+            #end
+        end
+    end
+    totint = cnt
+    #boundary reaion - bottom
+    for i = 1:nbnd
+        for j = 1:n1
+            cnt = cnt + 1
+            @inbounds coords[1:2, cnt] =
+                [(-x / 2) + dx / 2 + (j - 1) * dx, (-y / 2) - dx / 2- (i - 1) * dx]
+        end
+    end
+
+    totbottom = cnt
+    #boundary reaion - top
+    for i = 1:nbnd
+        for j = 1:n1
+            cnt = cnt + 1
+            @inbounds coords[1:2, cnt] =
+                [(-x / 2) + dx / 2 + (j - 1) * dx, (y / 2) + dx / 2 + (i - 1) * dx]
+        end
+    end
+    tottop = cnt
+
+    # r = dx / (4 * n1) * rand(size(coords)[1], size(coords)[2])
+    #coords = coords + r  #"random" pertubation of grid
+    return coords, totint, totbottom, tottop
+end
